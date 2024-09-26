03b73333a49420a5acb2385395f72313b89b9559
Your Name <you@example.com>
2024-09-26 23:52:30 +0300

    Add JS scripts

---
+++ scripts/My.js
@@ -0,0 +1,29 @@
+
+const myName = "Danylo"
+console.log(typeof name);
+const myAge = 5;
+console.log(typeof myAge);
+const nothing = null;
+console.log(typeof nothing);
+let myPhoneNumber;
+console.log(typeof myPhoneNumber);
+const parameter = {
+    gender: "male",
+    height: "180cm",
+    hobbies: "Gym"
+}
+console.log(typeof parameter);
+const myNameIsDanylo = true;
+console.log(typeof myNameIsDanylo);
+function sum (){
+    const a = 543;
+    const b = 22.5;
+    const multiple = a * b;
+    console.log(multiple)
+}
+console.log(typeof sum);
+
+
+const name = prompt("What is your name?");
+const email = prompt("What is your email address?");
+const location = prompt("What is your location address?");
