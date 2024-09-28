e902bdf1b955aad039d5dba387bb7f9b19e39187
Your Name <you@example.com>
2024-09-28 14:05:09 +0300

    Add Scripts

--- scripts/My.js
+++ scripts/My.js
@@ -1,6 +1,6 @@
-
+"use strict";
 const myName = "Danylo"
-console.log(typeof name);
+console.log(typeof myName);
 const myAge = 5;
 console.log(typeof myAge);
 const nothing = null;
@@ -18,12 +18,22 @@ console.log(typeof myNameIsDanylo);
 function sum (){
     const a = 543;
     const b = 22.5;
-    const multiple = a * b;
+    const multiple = a + b;
     console.log(multiple)
 }
 console.log(typeof sum);
 
 
-const name = prompt("What is your name?");
-const email = prompt("What is your email address?");
-const location = prompt("What is your location address?");
+const yourName = prompt("Define your name")
+const yourPhoneNumber = prompt("Insert your phone number")
+const yourCountryOfResidence = prompt("Your country of residence")
+console.log("${yourName },${yourPhoneNumber},${yourCountryOfResidence}")
+
+
+function splitName (digit){
+    let divide = digit.toString().split("");
+
+    console.log(divide.join(" "))
+}
+splitName(10369);
+
